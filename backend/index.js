c6d210d7652d2a6a291a86724ea598316412afea
PP <nicolepan1023@163.com>
2024-11-17 21:53:44 +0900

    Update all products

--- backend/index.js
+++ backend/index.js
@@ -23,10 +23,10 @@ app.get("/", (req, res)=>{
 const storage = multer.diskStorage({
   destination: './upload/images',
   filename: (req, file, cb)=>{
-    return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
   }
 })
-const upload = multer({storage: storage})
+const upload = multer({storage: storage});
 
 // Create upload endpoint for images
 app.use('/images', express.static('upload/images'))
