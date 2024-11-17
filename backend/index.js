ac132ea3652885fe1fb0903a1bab9088e5a17558
PP <nicolepan1023@163.com>
2024-11-18 00:13:14 +0900

    Add backend for login and signup

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
-const { log } = require("console");
+const { log, error } = require("console");
 
 app.use(express.json());
 app.use(cors());
@@ -17,6 +17,76 @@ mongoose.connect("mongodb+srv://celestepan1023:gs211023@cluster0.sstpk.mongodb.n
 // API creation
 app.get("/", (req, res)=>{
     res.send("Express app is running.")
+})
+
+// Schema creating for user model
+const Users = mongoose.model('Users', {
+  name:{
+    type: String,
+  },
+  email:{
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  cartData: {
+    type: Object,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  }
+})
+
+// Create endpoint for user registration
+app.post('/signup', async(req, res)=>{
+  let check = await Users.findOne({email: req.body.email});
+  if (check) {
+    return res.status(400).json({success: false, error: "User with the same email addrss exists."})
+  }
+  let cart = {};
+  for (let i = 0; i < 300; i++) {
+    cart[i] = 0;
+  }
+  const user = new Users({
+    name: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    cartData: cart,
+  })
+  await user.save();
+
+  const data = {
+    user: {
+      id: user.id
+    }
+  }
+
+  const token = jwt.sign(data, 'secret_ecom');
+  res.json({success: true, token})
+})
+
+//Create endpoint for user login
+app.post('/login', async (req, res)=>{
+  let user = await Users.findOne({email: req.body.email});
+  if (user){
+    const passCompare = req.body.password === user.password;
+    if (passCompare){
+      const data = {
+        user:{
+          id: user.id,
+        }
+      }
+      const token = jwt.sign(data, 'secret-ecom');
+      res.json({success: true, token});
+    }else{
+      res.json({success: false, errors: "Wrong Password"})
+    }
+  }else{
+    res.json({success: false, errors: "Wrong Email Id"})
+  }
 })
 
 // Image storage engine
@@ -99,7 +169,7 @@ app.post('/addproduct', async(req, res)=>{
   console.log("Saved")
   res.json({
     success: true,
-    nmae: req.body.name,
+    name: req.body.name,
   })
 })
 
